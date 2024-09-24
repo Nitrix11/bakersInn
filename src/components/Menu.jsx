4837f13275a2ce8cfa70cc09d2746cb79c30a9d7
Nitrix11 <ethanpeetro@gmail.com>
2024-09-24 10:07:03 +0200

    #

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,6 +1,7 @@
-import React, { useState } from 'react';
-import "../components/Menu.css"
+import "../components/Menu.css";
 import { Link } from "react-router-dom";
+import { useState } from "react";
+
 const HamburgerMenu = () => {
     const [isOpen, setIsOpen] = useState(false);
 
@@ -10,33 +11,27 @@ const HamburgerMenu = () => {
 
     return (
         <div className="hamburger-menu">
-            <div id="top" className='flex'>
-            <div className={`circle1 blue1 ${isOpen ? 'open' : ''}`} onClick={toggleMenu}></div>
-            <div className={`circle1 brown1 ${isOpen ? 'open' : ''}`} onClick={toggleMenu}></div>
+            <div className="flex" onClick={toggleMenu}>
+                <div className={`circle1 blue1 ${isOpen ? 'open' : ''}`}></div>
+                <div className={`circle1 brown1 ${isOpen ? 'open' : ''}`}></div>
+                <div className={`circle1 brown1 ${isOpen ? 'open' : ''}`}></div>
+                <div className={`circle1 blue1 ${isOpen ? 'open' : ''}`}></div>
             </div>
-            <div id="bottom" className='flex'>
-            <div className={`circle1 brown1 ${isOpen ? 'open' : ''}`} onClick={toggleMenu}></div>
-            <div className={`circle1 blue1 ${isOpen ? 'open' : ''}`} onClick={toggleMenu}></div>
-            </div>
-           
-         
+
             {isOpen && (
                 <div className="menu-links">
-                   <Link to="about">
-                    <li id="li"><a href="" className="text-[#261B6C] text-[22px] font-bold">  About Us</a></li>
+                    <Link to="about">
+                        <li><a href="#" className="text-[#261B6C] text-[22px] font-bold">About Us</a></li>
                     </Link>
-                    
-                   <Link to='product'>
-                   <li><a href="" className="text-[#261B6C] text-[22px] font-bold" >Products</a></li>
-                   </Link>
-                   <Link to="recipes">
-           
-                    <li><a href="" className="text-[#261B6C] text-[22px] font-bold" >Recipes</a></li>
+                    <Link to='product'>
+                        <li><a href="#" className="text-[#261B6C] text-[22px] font-bold">Products</a></li>
+                    </Link>
+                    <Link to="recipes">
+                        <li><a href="#" className="text-[#261B6C] text-[22px] font-bold">Recipes</a></li>
                     </Link>
                     <Link to="kids">
-                    <li><a href="" className="text-[#261B6C] text-[22px] font-bold" >Kid's Corner</a></li>
+                        <li><a href="#" className="text-[#261B6C] text-[22px] font-bold">Kid's Corner</a></li>
                     </Link>
-                    
                 </div>
             )}
         </div>
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,48 +1,57 @@
-import logo from"../assets/bakers-inn-category 1.png"
-import"../components/Nav.css"
-import HamburgerMenu from"../components/Menu"
+import React, { useState } from "react";
+import logo from "../assets/bakers-inn-category 1.png";
 import { Link } from "react-router-dom";
-function Nav(){
-    return(
-        <>
-        <nav id="nav" className="flex items-center justify-between height-[10px] sticky " >
-            <div className="logo ">
-                <Link to="/">
-                <img className="w-[65%]  h-[50%] pl-[15px]" src={logo} alt="" />     
-                </Link>
+import"../components/Nav.css"
 
-            </div>
-            <div id="links" className="flex items-center justify-between pr-[15px]">
-                <ul className="flex justify-center gap-[20px]"  >
-                    <Link to="about">
-                    <li id="li"><a href="" className="text-[#261B6C] text-[22px] font-bold">  About Us</a></li>
+const Nav = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <div className="nav-bar">
+      <div className="nav">
+        <div className="logo">
+          <Link to="/">
+            <img src={logo} alt="Baker's Inn" />
+          </Link>
+        </div>
+
+        <div className={`nav-links ${isOpen ? "open" : ""}`}>
+        <Link to="about">
+                        <li><a href="#" className="text-[#261B6C] text-[22px] font-bold">About Us</a></li>
                     </Link>
-                    
-                   <Link to='product'>
-                   <li><a href="" className="text-[#261B6C] text-[22px] font-bold" >Products</a></li>
-                   </Link>
-                   <Link to="recipes">
-           
-                    <li><a href="" className="text-[#261B6C] text-[22px] font-bold" >Recipes</a></li>
+                    <Link to='product'>
+                        <li><a href="#" className="text-[#261B6C] text-[22px] font-bold">Products</a></li>
+                    </Link>
+                    <Link to="recipes">
+                        <li><a href="#" className="text-[#261B6C] text-[22px] font-bold">Recipes</a></li>
                     </Link>
                     <Link to="kids">
-                    <li><a href="" className="text-[#261B6C] text-[22px] font-bold" >Kid's Corner</a></li>
+                        <li><a href="#" className="text-[#261B6C] text-[22px] font-bold">Kid's Corner</a></li>
                     </Link>
-                    
-                </ul>
-                <Link to='contact'>
+          <Link to="/contact">
+            <button>Contact Us</button>
+          </Link>
+        </div>
+
+        <div className="hamburger" onClick={toggleMenu}>
+            <div className="flex">
+            <div className={`circle1 blue1 ${isOpen ? 'open' : ''}`}></div>
+                <div className={`circle1 brown1 ${isOpen ? 'open' : ''}`}></div>
                
-                <button id="button11" className=" text-[#fff] rounded-[5px] font-bold bg-[#BA8F55] p-[10px] ml-[20px] ">CONTACT US</button>
-                </Link>
-                <div  id="Menu">
-                <HamburgerMenu  id="menu1"/>
-                </div>
-              
-              
             </div>
-           
-        </nav>
-        </>
-    );
-}
+            <div className="flex">
+            <div className={`circle1 brown1 ${isOpen ? 'open' : ''}`}></div>
+                <div className={`circle1 blue1 ${isOpen ? 'open' : ''}`}></div>
+            </div>
+      
+        </div>
+      </div>
+    </div>
+  );
+};
+
 export default Nav;
